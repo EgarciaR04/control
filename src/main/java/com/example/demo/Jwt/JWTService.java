ec87b4b2f92de3b97e28563b1bc6b10e0c4c7fcf
EricG <ericrivas192@gmail.com>
2024-10-17 18:58:27 -0600

    Confirmacion para version final de proycto

--- src/main/java/com/example/demo/Jwt/JWTService.java
+++ src/main/java/com/example/demo/Jwt/JWTService.java
@@ -33,7 +33,7 @@ public class JWTService {
             .setClaims(extraClaims)
             .setSubject(user_.getUsername())
             .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
             .signWith(getKey(), SignatureAlgorithm.HS256)
             .compact();
     }
